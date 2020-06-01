7767b0b6a967f8c15e17179a0077cf64d0b7ef77
Aditya-chetri <64538450+Aditya-chetri@users.noreply.github.com>
2020-06-01 18:55:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,28 +8,28 @@ function setup() {
   weight = random(400, 1500);
   car = createSprite(50, 200, 50, 50);
   car.velocityX = speed;
-  wall = createSprite(500, 200, 60, height/2);
+  wall = createSprite(1500, 200, 60, height/2);
 } 
  
 function draw() {
   background(255,255,255);  
   
-  if (car.collide(wall)){
-    deformation();
+  if (wall.x-car.x < (car.width+wall.width)/2){
+    car.velocityX = 0;
+    var deformation = (0.5*weight*speed*speed)/22509;
+    if (deformation<100){
+      car.shapeColor = "green";
+    }
+    if (deformation>100&&deformation<180){
+      car.shapeColor = "yellow";
+    }
+    if (deformation>180){
+      car.shapeColor = "red";
+    }
   }
   
   drawSprites();
 }
 
-function deformation(){
-  deformation = 0.5*weight*speed*speed/22500;
-  if (deformation<100){
-    car.shapeColor = "green";
-  }
-  if (deformation>100&&deformation<180){
-    car.shapeColor = "yellow";
-  }
-  if (deformation>180){
-    car.shapeColor = "red";
-  }
-}
+
+  
